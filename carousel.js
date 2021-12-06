d589740d9548e772d878b28b2f2e6100d380f65e
disar <disarra2002@gmail.com>
2021-12-06 10:37:19 +0300

    ссылки в футере

--- carousel.js
+++ carousel.js
@@ -1,7 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-    if (window.screen.width) {
-        const slider = new ChiefSlider('.slider', {
-            loop: false
-          });
-    }
-  });
+  if (window.screen.width) {
+      const slider = new ChiefSlider('.slider', {
+          loop: false
+        });
+  }
+  setTimeout(upadateSlide, 3000,  2)
+});
+
+function upadateSlide(val) {
+  if (parseInt(val) < 5) {
+    console.log("switch" + val);
+    document.getElementById("switch" + val).checked = true
+    setTimeout(upadateSlide, 3000, val+1)
+  }
+  else {
+    document.getElementById("switch" + val).checked = true
+    setTimeout(upadateSlide, 3000,  1)
+  }
+}
