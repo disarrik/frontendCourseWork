1170aeea8fd134de40eb1411c1d23a3fe4850932
disar <disarra2002@gmail.com>
2021-12-06 11:56:57 +0300

    Изменил поиск

--- search.js
+++ search.js
@@ -1,14 +1,39 @@
 $(document).ready(function () {
-    $("#term").keypress(function () {
+    $("#term").keyup (function () {
       var term = "";
       term = $('#term').val();
       $('#content').unhighlight();
-      console.log(term);
-      if ($('#term').val() == "") {
-        // $('#content').addClass("highlight")
-      } else {
+      var elem = document.getElementById("content");
+      if ($('#term').val() != "") {
         $('#content').highlight(term);
-        return true;
+        for(var i = 0; i < elem.children.length; i++) {
+          if (myHasClass(elem.children[i], "highlight")) {
+            elem.children[i].style.display = "block";
+          }
+          else {
+            elem.children[i].style.display = "none";
+          }
+        }
       }
+      else {
+        for(var i = 0; i < elem.children.length; i++) {
+          elem.children[i].style.display = "block";
+        }
+      }
+      return true;
     });
   });
+
+function myHasClass(elem, className) {
+  if (elem.children.length == 0) {
+    return elem.classList.contains(className);
+  }
+  else {
+    var flag = false
+    for(var i = 0; i < elem.children.length; i++) {
+      var flag = myHasClass(elem.children[i], className)
+      if (flag) break
+    }
+    return (elem.classList.contains(className) || flag)
+  }
+}
